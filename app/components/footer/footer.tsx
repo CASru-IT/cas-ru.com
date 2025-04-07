f251e83e1765c34c3948779dabeafe9af009cacc
dugite-git <138967966+dugite-git@users.noreply.github.com>
2025-04-07 05:12:51 +0000

    コミット

---
+++ app/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.scss'; // SCSSファイルをインポート
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Your Company. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
