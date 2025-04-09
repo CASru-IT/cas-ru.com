14e2a178b32d429858a450ca4819d59db0b9ac8d
dugite-git <138967966+dugite-git@users.noreply.github.com>
2025-04-09 18:38:25 +0900

    修正中

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import './footer.scss'; // SCSSファイルをインポート
+import React from "react";
+import "./footer.scss"; // SCSSファイルをインポート
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Your Company. All rights reserved.</p>
+      <div className="footer_columns">
+        <div className="footer_column footer_column_highlight">
+          <ul>
+            <li>Top</li>
+            <li>About</li>
+            <li>Join us</li>
+            <li>Groups</li>
+            <li>Articles</li>
+          </ul>
+        </div>
+        <div className="footer_column">
+          <ul>
+            <li>トップページ</li>
+            <li>CASるについて</li>
+            <li>参加方法</li>
+            <li>班一覧</li>
+            <li>記事一覧</li>
+          </ul>
+        </div>
+        <div className="footer_column">
+          <ul>
+            <li>CASる</li>
+            <li>DTM班</li>
+            <li>IT班</li>
+            <li>ゲーム班</li>
+          </ul>
+        </div>
+        <div className="footer_column">
+          <ul>
+            <li>CASる</li>
+          </ul>
+        </div>
+      </div>
+      <p>&copy; 2025 CASる</p>
     </footer>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import Footer from "./components/footer/footer";
 // import styles from "./page.module.scss";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <Footer />
+    </main>
+  );
 }
